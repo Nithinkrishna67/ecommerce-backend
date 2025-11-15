fa217771c2338a7604a62ddfe8f0da427e6dc081
Nithin Krishna <nithinkrishna235@gmail.com>
2025-11-15 15:24:38 +0530

    add the cors

--- server.js
+++ server.js
@@ -25,7 +25,11 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "https://nithinkrishna67.github.io", 
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: false
+}));
 app.use(express.json());
 
 // Serve images from the images folder
